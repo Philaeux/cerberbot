607f3d1b41c31031b67576ce072120e52f4b41ef
Vincent 'Philaeux' Lamotte <vinc.lamotte@gmail.com>
2023-11-06 20:16:44 +0100

    Fix stratz fetch match info

--- src/cerberbot/cerberbot.py
+++ src/cerberbot/cerberbot.py
@@ -20,24 +20,24 @@ class CerberBot:
     async def fetch_player_history(self):
         game_counts = {}
         async with aiohttp.ClientSession() as session:
-            async with session.get(f"https://api.opendota.com/api/players/{self.streamer_id}/matches?date=30") as response:
+            async with session.get(
+                    f"https://api.opendota.com/api/players/{self.streamer_id}/matches?date=30") as response:
                 game_list = await response.json()
                 print(f"Detected {len(game_list)} games in the past 30 days.")
 
-        headers = {'Authorization': f"Bearer {self.cfg['Default']['STRATZ_TOKEN']}"}
-        async with aiohttp.ClientSession(headers=headers)as session:
+        headers = {'Content-Type': 'application/json',
+                   'Authorization': f"Bearer {self.cfg['Default']['STRATZ_TOKEN']}"}
+        async with aiohttp.ClientSession(headers=headers) as session:
             for index, game in enumerate(game_list):
-                print(game['match_id'])
                 print(f"> Game {index} processing")
                 # Skip games shorter than 10 min
                 if game['duration'] < 600:
                     continue
 
-                await asyncio.sleep(1) # Avoid OpenDota 429
-                params = {'query': "{match(id: " + str(game['match_id']) + ") { players { steamAccountId playerSlot }}}"}
-                async with session.get(f"https://api.stratz.com/graphql/", params=params) as response:
-                    text = await response.text()
-                    print(text)
+                await asyncio.sleep(1)  # Avoid OpenDota 429
+                params = {
+                    'query': "{ match(id: " + str(game['match_id']) + ") { players { steamAccountId playerSlot }} }"}
+                async with session.get(f"https://api.stratz.com/graphql", params=params) as response:
                     details = await response.json()
                     match = details["data"]["match"]
                     if "players" not in match:
@@ -45,20 +45,22 @@ class CerberBot:
                     for player in match['players']:
                         # Skip if the player is not in the same team, or it's the Bulldog account
                         if ('steamAccountId' not in player
-                            or player['steamAccountId'] == None
-                            or player['steamAccountId'] == self.streamer_id
-                            or abs(player['playerSlot'] - game['player_slot']) > 5):
+                                or player['steamAccountId'] is None
+                                or player['steamAccountId'] == self.streamer_id
+                                or abs(player['playerSlot'] - game['player_slot']) > 5):
                             continue
 
                         if player['steamAccountId'] not in game_counts:
                             game_counts[player['steamAccountId']] = GameCount(player['steamAccountId'])
                         else:
-                            game_counts[player['steamAccountId']].count = game_counts[player['steamAccountId']].count + 1
+                            game_counts[player['steamAccountId']].count = game_counts[
+                                                                              player['steamAccountId']].count + 1
 
         sorted_counts = sorted(game_counts.values(), key=lambda x: x.count, reverse=True)
         for player in list(sorted_counts):
             print(f"{player.count} games for {player.id}")
         return sorted_counts
+
 
 class GameCount:
     def __init__(self, id):
